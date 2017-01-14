980b38d95dd09afed23251c1f3a89a96ae15ea3e
theepicsnail <theepicsnail@gmail.com>
2017-01-14 15:44:11 -0800

    Fixing loader.lua

--- loader.lua
+++ loader.lua
@@ -1,8 +1,7 @@
 local loader = {}
 
-
 function ls(dir)
-  -- List all the files in a directory as a table.
+  -- List all the files in a directory as an array.
   local results = {}
   for entry in io.popen('ls -1 ' .. dir):lines() do
     table.insert(results, entry)
@@ -11,35 +10,32 @@ function ls(dir)
 end
 
 function enemyFilename(level, enemy)
-	return "levels/" .. level .. "/enemies/" .. enemy
+  return "levels/" .. level .. "/enemies/" .. enemy
 end
- 
+
 function itemFilename(level, name)
-	return "levels/" .. level .. "/items/" .. name
+  return "levels/" .. level .. "/items/" .. name
 end
- 
+
 function loadEnemy(levelName, enemyName)
-   return dofile(enemyFilename(levelName, enemyName))
+  return dofile(enemyFilename(levelName, enemyName))
 end
- 
+
 function loadItem(levelName, itemName)
-   return dofile(itemFilename(levelName, itemName))
+  return dofile(itemFilename(levelName, itemName))
 end
 
 function loadEnemies(levelName)
   -- Loads the enemies into a table and returns that
-	local enemies = {}
-	for _, name in pairs(ls("levels/" .. levelName .. "/enemies")) do
-		table.insert(enemies, loadEnemy(levelName, name))
-	end
-	return enemies
+  local enemies = {}
+  for _, name in pairs(ls("levels/" .. levelName .. "/enemies")) do
+    table.insert(enemies, loadEnemy(levelName, name))
+  end
+  return enemies
 end
 
 function loadItems(levelName)
   -- Loads all the items from a level and returns a table of them.
-	local level = {name = name}
-	level["enemies"] = loadEnemies(name)
-	return level
 end
 
 function loader.listLevels()
@@ -48,13 +44,13 @@ function loader.listLevels()
 end
 
 function loader.loadLevel(name)
-  --[[ 
-	Loads a level, returning a table like: {
-	enemies = {table of enemies}
-	items = {table of items
-	}
-	]]--
-
+  -- Loads a level, returning a table like: {
+  -- enemies = {table of enemies}
+  -- items = {table of items
+  -- }
+  local level = {name = name}
+  level["enemies"] = loadEnemies(name)
+  return level
 end
 
 
