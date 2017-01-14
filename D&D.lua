74567988df1209828dde74cf0d405c095566c894
xXDerpyXx <bottledcanofdoom@gmail.com>
2017-01-14 17:13:23 -0500

    Starting off with the right foot

---
+++ D&D.lua
@@ -0,0 +1,35 @@
+function load()
+  -- read all the levels
+  print("LOADING CONTENT")
+  levels = ls("levels")
+  levelEnemies = {}
+  enemyNums = {}
+  levelItems={}
+  levelCount=0
+  for _,level in ipairs(levels) do
+	levelCount=levelCount+1
+    levelEnemies[level] = {}
+    levelItems[level] = {}
+      -- for each level, read the enemies
+      tempNum = 1
+    for _,enemy in ipairs(ls("levels/" .. level .. "/enemies")) do
+      table.insert(levelEnemies[level], enemy)
+    end
+    for _,item in ipairs(ls("levels/" .. level .. "/items")) do
+      table.insert(levelItems[level], item)
+    end
+  end
+
+  -- Print out what we loaded
+	for _,level in ipairs(levels) do
+		print(level..":")
+		print("   Enemies:")
+		for _,enemy in ipairs(levelEnemies[level]) do
+			print("     " .. enemy)
+		end
+		print("   Items:")
+		for _,item in ipairs(levelItems[level]) do
+			print("     " .. item)
+		end
+	end
+end
