1ce758bd5857353b44f003294fe8f01043ac410f
xXDerpyXx <bottledcanofdoom@gmail.com>
2017-01-14 18:52:40 -0500

    loader_test.lua tweaks, the sequal

--- loader_test.lua
+++ loader_test.lua
@@ -1,14 +1,14 @@
 local loader = require "loader"
 function dump(tbl, indent)
-  local indent = indent or ""
-  for k,v in pairs(tbl) do
-    if type(v) == "table" then
-      print(indent .. k..":")
-      dump(v, indent .. "  ")
-    else
-      print(indent .. k .. "=" .. v)
-    end
-  end
+	local indent = indent or ""
+	for k,v in pairs(tbl) do
+		if type(v) == "table" then
+			print(indent .. k..":")
+			dump(v, indent .. "  ")
+		else
+			print(indent .. k .. "=" .. v)
+		end
+	end
 end
 
 print "Levels detected"
