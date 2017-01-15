6276a11585c5a69e618a726eb51a4fc0a77924fb
xXDerpyXx <bottledcanofdoom@gmail.com>
2017-01-15 00:37:58 -0500

    cleanup!

--- title.lua
+++ title.lua
@@ -1,15 +1,26 @@
 function displayTitle()
 	print "#######                   #######"
+	os.execute("sleep .1")
 	print " #     #      ####         #     #"
+	os.execute("sleep .1")
 	print " #      #    #    #        #      #"
+	os.execute("sleep .1")
 	print " #      #    #    #        #      #"
+	os.execute("sleep .1")
 	print " #      #     #  #         #      #"
+	os.execute("sleep .1")
 	print " #      #      ##          #      #"
+	os.execute("sleep .1")
 	print " #      #     #  #         #      #"
+	os.execute("sleep .1")
 	print " #      #    #    # #      #      #" 
+	os.execute("sleep .1")
 	print " #      #    #     ##      #      #"
+	os.execute("sleep .1")
 	print " #      #    #      #      #      #"
+	os.execute("sleep .1")
 	print " #     #     #     # #     #     #"
+	os.execute("sleep .1")
 	print "#######       #####   #   #######"
 	print "          THE REVENGENCE"
 end
