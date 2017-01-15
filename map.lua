c3c36bd3fa9b2edf49f6e6968d68ea5ad2624e53
theepicsnail <theepicsnail@gmail.com>
2017-01-14 20:07:10 -0800

    Map cleanup

--- map.lua
+++ map.lua
@@ -21,24 +21,18 @@ local function inBounds(bounds, x, y)
 end
 
 local function randomWalk(x, y,map)
-	while true do
-		local rnd = math.random(1, 4)
-		if rnd == 1 and (mapGet(map,x,y+2) ~="O" or math.random(1,50) == 25) then
-			mapSet(map, x, y+1, "O")
-			return x, y + 2
-		end
-		if rnd == 2 and (mapGet(map,x,y-2) ~="O" or math.random(1,50) == 25) then
-			mapSet(map, x, y-1, "O")
-			return x, y - 2
-		end
-		if rnd == 3 and (mapGet(map,x+2,y) ~="O" or math.random(1,50) == 25) then
-			mapSet(map, x+1, y, "O")
-			return x + 2, y
-		end
-		if rnd == 4 and (mapGet(map,x-2,y) ~="O" or math.random(1,50) == 25) then
-			mapSet(map, x-1, y, "O")
-			return x - 2, y
-		end
+	local rnd = math.random(1, 4)
+	if rnd == 1 then
+		return x, y + 1, x, y + 2
+	end
+	if rnd == 2 then
+		return  x, y - 1, x, y - 2
+	end
+	if rnd == 3 then
+		return x + 1, y,  x + 2, y
+	end
+	if rnd == 4 then
+		return x - 1, y, x - 2, y
 	end
 	return x, y -- Shouldn't be possible? [its not possible]
 end
@@ -47,16 +41,21 @@ function module.create(bounds)
 	local map = {bounds = bounds}
 	local currentPosY = 0
 	local currentPosX = 0
+	mapSet(map, 0, 0, "O")
 	for i = 1,50 do
 		-- Walk around (in bounds) until we find a non "O"
-		while mapGet(map, currentPosX, currentPosY) == "O" do
-			local nextX, nextY = randomWalk(currentPosX, currentPosY,map)
-			if inBounds(bounds, nextX, nextY) then
-				currentPosX = nextX
-				currentPosY = nextY
+		local done = false
+		while not done do
+			local x1, y1, x2, y2 = randomWalk(currentPosX, currentPosY,map)
+			if inBounds(bounds, x2, y2) then
+				if mapGet(map, x2, y2) ~= "O" or math.random() < .05 then
+					mapSet(map, x1, y1, "O")
+					mapSet(map, x2, y2, "O")
+					done = true
+					currentPosX, currentPosY = x2, y2
+				end
 			end
 		end
-		mapSet(map, currentPosX, currentPosY, "O")
 	end
 	return map
 end
