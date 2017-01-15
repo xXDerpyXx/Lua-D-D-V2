264e20bfcc6de8409d3789b09e2718b2c41b6118
xXDerpyXx <bottledcanofdoom@gmail.com>
2017-01-14 22:46:09 -0500

    map bugfixing!

--- map.lua
+++ map.lua
@@ -20,28 +20,33 @@ local function inBounds(bounds, x, y)
 	return true
 end
 
-local function randomWalk(x, y)
-	local rnd = math.random(1, 4)
-	if rnd == 1 then
-		return x, y + 1
-	elseif rnd == 2 then
-		return x, y - 1
-	elseif rnd == 3 then
-		return x + 1, y
-	elseif rnd == 4 then
-		return x - 1, y
+local function randomWalk(x, y,map)
+	while true do
+		local rnd = math.random(1, 4)
+		if rnd == 1 and not mapGet(map,x,y+2) =="O" then
+			return x, y + 2
+		end
+		if rnd == 2 and not mapGet(map,x,y-2) =="O" then
+			return x, y - 2
+		end
+		if rnd == 3 and not mapGet(map,x+2,y) =="O" then
+			return x + 2, y
+		end
+		if rnd == 4 and not mapGet(map,x-2,y) =="O" then
+			return x - 2, y
+		end
 	end
-	return x, y -- Shouldn't be possible?
+	return x, y -- Shouldn't be possible? [its not possible]
 end
 
 function module.create(bounds)
 	local map = {bounds = bounds}
 	local currentPosY = 0
 	local currentPosX = 0
-	for i = 1,50 do
+	for i = 1,2 do
 		-- Walk around (in bounds) until we find a non "O"
 		while mapGet(map, currentPosX, currentPosY) == "O" do
-			local nextX, nextY = randomWalk(currentPosX, currentPosY)
+			local nextX, nextY = randomWalk(currentPosX, currentPosY,map)
 			if inBounds(bounds, nextX, nextY) then
 				currentPosX = nextX
 				currentPosY = nextY
