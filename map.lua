0a021a0bb07ba31cf6fd59bc39429274e75e41d0
xXDerpyXx <bottledcanofdoom@gmail.com>
2017-01-14 22:55:30 -0500

    map bugfixing!

--- map.lua
+++ map.lua
@@ -23,16 +23,20 @@ end
 local function randomWalk(x, y,map)
 	while true do
 		local rnd = math.random(1, 4)
-		if rnd == 1 and not mapGet(map,x,y+2) =="O" then
+		if rnd == 1 and (mapGet(map,x,y+2) ~="O" or math.random(1,50) == 25) then
+			mapSet(map, x, y+1, "O")
 			return x, y + 2
 		end
-		if rnd == 2 and not mapGet(map,x,y-2) =="O" then
+		if rnd == 2 and (mapGet(map,x,y-2) ~="O" or math.random(1,50) == 25) then
+			mapSet(map, x, y-1, "O")
 			return x, y - 2
 		end
-		if rnd == 3 and not mapGet(map,x+2,y) =="O" then
+		if rnd == 3 and (mapGet(map,x+2,y) ~="O" or math.random(1,50) == 25) then
+			mapSet(map, x+1, y, "O")
 			return x + 2, y
 		end
-		if rnd == 4 and not mapGet(map,x-2,y) =="O" then
+		if rnd == 4 and (mapGet(map,x-2,y) ~="O" or math.random(1,50) == 25) then
+			mapSet(map, x-1, y, "O")
 			return x - 2, y
 		end
 	end
@@ -43,7 +47,7 @@ function module.create(bounds)
 	local map = {bounds = bounds}
 	local currentPosY = 0
 	local currentPosX = 0
-	for i = 1,2 do
+	for i = 1,50 do
 		-- Walk around (in bounds) until we find a non "O"
 		while mapGet(map, currentPosX, currentPosY) == "O" do
 			local nextX, nextY = randomWalk(currentPosX, currentPosY,map)
