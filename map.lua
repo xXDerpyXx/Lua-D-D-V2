9d807ed0c90f4de72540bc972df15b824aeb51dd
xXDerpyXx <bottledcanofdoom@gmail.com>
2017-01-14 23:04:33 -0500

    map bugfixing!

--- map.lua
+++ map.lua
@@ -21,24 +21,22 @@ local function inBounds(bounds, x, y)
 end
 
 local function randomWalk(x, y,map)
-	while true do
-		local rnd = math.random(1, 4)
-		if rnd == 1 and (mapGet(map,x,y+2) ~="O" or math.random(1,50) == 25) then
-			mapSet(map, x, y+1, "O")
-			return x, y + 2
-		end
-		if rnd == 2 and (mapGet(map,x,y-2) ~="O" or math.random(1,50) == 25) then
-			mapSet(map, x, y-1, "O")
-			return x, y - 2
-		end
-		if rnd == 3 and (mapGet(map,x+2,y) ~="O" or math.random(1,50) == 25) then
-			mapSet(map, x+1, y, "O")
-			return x + 2, y
-		end
-		if rnd == 4 and (mapGet(map,x-2,y) ~="O" or math.random(1,50) == 25) then
-			mapSet(map, x-1, y, "O")
-			return x - 2, y
-		end
+	local rnd = math.random(1, 4)
+	if rnd == 1 and (mapGet(map,x,y+2) ~="O" or math.random(1,50) == 25) then
+		mapSet(map, x, y+1, "O")
+		return x, y + 2
+	end
+	if rnd == 2 and (mapGet(map,x,y-2) ~="O" or math.random(1,50) == 25) then
+		mapSet(map, x, y-1, "O")
+		return x, y - 2
+	end
+	if rnd == 3 and (mapGet(map,x+2,y) ~="O" or math.random(1,50) == 25) then
+		mapSet(map, x+1, y, "O")
+		return x + 2, y
+	end
+	if rnd == 4 and (mapGet(map,x-2,y) ~="O" or math.random(1,50) == 25) then
+		mapSet(map, x-1, y, "O")
+		return x - 2, y
 	end
 	return x, y -- Shouldn't be possible? [its not possible]
 end
@@ -47,7 +45,7 @@ function module.create(bounds)
 	local map = {bounds = bounds}
 	local currentPosY = 0
 	local currentPosX = 0
-	for i = 1,50 do
+	for i = 1,100 do
 		-- Walk around (in bounds) until we find a non "O"
 		while mapGet(map, currentPosX, currentPosY) == "O" do
 			local nextX, nextY = randomWalk(currentPosX, currentPosY,map)
@@ -58,6 +56,7 @@ function module.create(bounds)
 		end
 		mapSet(map, currentPosX, currentPosY, "O")
 	end
+	mapSet(map, 0, 0, "X")
 	return map
 end
 
--- map_test.lua
+++ map_test.lua
@@ -2,10 +2,10 @@ local map = require "map"
 
 math.randomseed(os.time())
 size = {}
-size["maxX"] = 15
-size["maxY"] = 15
-size["minX"] = -15
-size["minY"] = -15
+size["maxX"] = 20
+size["maxY"] = 20
+size["minX"] = -20
+size["minY"] = -20
 
 myMap = map.create(size)
 map.display(myMap)
