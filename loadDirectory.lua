43c35d136f0366c0e0b7bffcb17433332ad0f47d
xXDerpyXx <bottledcanofdoom@gmail.com>
2017-01-16 17:39:08 -0500

    OS Autodetect

--- loadDirectory.lua
+++ loadDirectory.lua
@@ -2,44 +2,46 @@ local module = {}
 
 local HostOS = "linux"
 
+--[[
 function module.autoDetect()
 	local output = "Linux"
 	output = io.popen("uname")
 	if output == "Linux" then
-		output = "Linux"
+		output = "linux"
 	else
 		output = "windows"
 	end
 	return output
 end
+]]--
+
 
 function module.getOS()
-	HostOS = module.autoDetect()
 	return HostOS
 end
 
 function module.changeOS(OS)
-  -- Change the method this script will use to list a directory
-  if string.lower(OS) == ("windows" or "win") then
-    HostOS = "windows"
-  elseif string.lower(OS) == "linux" then
-    HostOS = "linux"
-  end
+	-- Change the method this script will use to list a directory
+	if string.lower(OS) == ("windows" or "win") then
+		HostOS = "windows"
+	elseif string.lower(OS) == "linux" then
+		HostOS = "linux"
+	end
 end
 
 function module.loadDir(dir)
-  -- List all the files in a directory as an array.
-  local results = {}
-  local miniOS
-  if module.getOS() == "windows" then
-    method = 'dir "' .. dir .. '" /b'
-  else
-    method = 'ls  -1 ' .. dir
-  end
-  for entry in io.popen(method):lines() do
-    table.insert(results, entry)
-  end
-  return results
+	-- List all the files in a directory as an array.
+	local results = {}
+	local miniOS
+	if module.getOS() == "windows" then
+		method = 'dir "' .. dir .. '" /b'
+	else
+		method = 'ls  -1 ' .. dir
+	end
+	for entry in io.popen(method):lines() do
+		table.insert(results, entry)
+	end
+	return results
 end
 
 return module
