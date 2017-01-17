bad46f6bf97250dc1dbe7a0be93e68978b6eb306
xXDerpyXx <bottledcanofdoom@gmail.com>
2017-01-16 23:10:51 -0500

    SAVE+LOAD

--- D&D.lua
+++ D&D.lua
@@ -10,7 +10,50 @@ local player = require("player")
 menuInput = nil
 
 function playerCreation()
-	
+	local stats = {}
+	io.write("Name:")
+	input = io.read("*line")
+	stats["name"] = input
+	print("")
+	print("Pick a class")
+	print("Rogue -- Very rounded, though not particularly good at much.")
+	print("Brute -- Strong as an Ox, though, also dumb as one.")
+	print("Mage -- Smart and good at sorcery, but weak and cowardly.")
+	print("Knight -- Strong, brave, bold! but not the brightest or fastest.")
+	print("")
+	io.write(":")
+	input = io.read("*line")
+	if string.lower(input) == "rogue" then
+		player["defs"] = 5 --sharp defense
+		player["defb"] = 5 --blunt defense
+		player["defm"] = 5 --magic defense
+		player["dex"] = 5 --dexterity
+		player["str"] = 5 --strength
+		player["int"] = 5 --intelligence
+		player["spd"] = 5 --speed
+		player["luk"] = 5 --luck
+		player["xp"] = 0 --experience
+		player["lvl"] = 1 --level
+		player["mhp"] = 15 --maxHP
+		player["hp"] = 15 --HP
+	end
+	num = player.createPlayer(stats)
+	player.savePlayer(stats,num)
+	return stats
+end
+
+function loadGame()
+	print("Pick a player!")
+	allGames = loader.listGames()
+	for k,v in pairs(allGames) do
+		local tempPlayer = player.loadPlayer(k)
+		print(k.."   "..tempPlayer["name"])
+	end
+	while allGames[tonumber(input)] == nil do
+		io.write("Player:")
+		input = io.read("*line")
+	end
+	return player.loadPlayer(tonumber(input))
 end
 
 
@@ -30,10 +73,13 @@ while menuInput ~="5" do -- main loop!
 			menuInput = io.read("*line")
 		end
 		if menuInput == "2" then
-		
+			stats = playerCreation()
 		end
 	end
-	if menuInput ~= "5" then
+	if menuInput == "1" then
+		stats = loadGame()
+	end
+	if menuInput == "2" or menuInput == "1" then
 		while true do -- main game loop!
 			
 		end
